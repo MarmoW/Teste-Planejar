06754085900130c38944322290ff0c25e0464ff0
MarmoW <115847012+MarmoW@users.noreply.github.com>
2024-02-01 21:29:51 -0300

    feat: Backend Route and Frontend Form
    Forgot the earlier commits

---
+++ Front/planejar-front/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <DateForm msg="insira os horários"/>
+</template>
+
+<script>
+import DateForm from './components/DateForm.vue';
+
+export default {
+  name: 'App',
+  components: {
+    DateForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Front/planejar-front/src/components/DateForm.vue
@@ -0,0 +1,53 @@
+<!-- DateForm.vue -->
+<template>
+    <div>
+      <h2>Horários</h2>
+      <form @submit.prevent="submitForm">
+        <label for="timestamp">Hora Entrada:</label>
+        <input type="datetime-local" v-model="formData.d1" id="d1" required>
+  
+        <label for="timestamp">Hora Saída:</label>
+        <input type="datetime-local" v-model="formData.d2" id="d2" required>
+  
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        formData: {
+          d1: 'd1',
+          d2: 'd1'
+        }
+      };
+    },
+    methods: {
+      submitForm() {
+        console.log(this.formData);
+        axios.post('http://localhost:8000/api/hours', this.formData)
+        .then(response => {
+          console.log('Form submitted successfully:', response.data);
+        })
+        .catch(error => {
+          if (axios.isAxiosError(error)) {
+            console.error('AxiosError:', error);
+            console.error('Error response:', error.response);
+          } else {
+            console.error('Non-AxiosError:', error);
+          }
+        });
+      }
+    }
+  };
+
+  </script>
+  
+  <style scoped>
+  /* Add your component-specific styles here */
+  </style>
+  
